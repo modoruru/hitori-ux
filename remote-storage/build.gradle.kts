3984337a561ab8a7e800f3680c160f63357f233b
justlofe <lofikpc@gmail.com>
2026-07-03 10:53:45 +0700

    shade json and websockets into remote-storage

--- remote-storage/build.gradle.kts
+++ remote-storage/build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
     id("java-library")
+    id("com.gradleup.shadow").version("9.0.0-beta4")
 }
 
 dependencies {
-    api("org.java-websocket:Java-WebSocket:${properties["websocket_version"]}")
-    api("org.json:json:20251224")
+    implementation("org.java-websocket:Java-WebSocket:${properties["websocket_version"]}")
+    implementation("org.json:json:20251224")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
@@ -13,17 +14,31 @@ dependencies {
     testImplementation("org.xerial:sqlite-jdbc:3.53.2.0")
 }
 
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+
+    shadowJar {
+        archiveClassifier.set("")
+    }
+
+    jar {
+        enabled = false
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+}
+
 extensions.configure<PublishingExtension> {
     publications {
         create<MavenPublication>("maven") {
             artifactId = "remote-storage"
 
-            artifact(tasks.named("jar"))
+            artifact(tasks.named("shadowJar"))
             artifact(tasks.named("sourcesJar"))
         }
     }
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
